fb12dfa675c081b4815d24d2eac701a593c7ae1c
Mikhail Bortnyk <vessimir@gmail.com>
2015-11-09 01:54:27 +0200

    Fixed config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 require 'smart'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :smart, '9c3659734570f11fae842f34d13d3762', '51a2504a56831393e6c30eb86317fbd534e28bebc34e3d79d58dd5ccc0f3db8bc90f491d01a51b751feeee87a1a4a4d1b7bd6848695733d53ff35809f4890330', provider_ignores_state: true
+  key = ENV['SMART_KEY'] || '9c3659734570f11fae842f34d13d3762'
+  secret  = ENV['SMART_SECRET'] || '51a2504a56831393e6c30eb86317fbd534e28bebc34e3d79d58dd5ccc0f3db8bc90f491d01a51b751feeee87a1a4a4d1b7bd6848695733d53ff35809f4890330'
+  provider :smart, key, secret, provider_ignores_state: true
 end
