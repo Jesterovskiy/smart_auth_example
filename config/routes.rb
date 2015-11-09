6abcc48c581ee6a94960c29131ca6bb1693eaf14
Andriy Savchenko <andrey@aejis.eu>
2015-11-09 02:55:51 +0200

    Add button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: redirect('/auth/smart')
+  root to: 'home#index'
 
   match '/auth/smart/callback', to: 'authorizations#smart', via: [:get, :post]
   match '/auth/failure', to: 'authorizations#failure', via: :get
